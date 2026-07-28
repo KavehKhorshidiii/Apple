626bd589853ac4d8b3835c808c18afe2f3a63de3
Kaveh <kavehkhorshidiii@gmail.com>
2026-07-28 20:29:55 +0330

    developing part of the top bar

--- app/components/layout/TopBar.tsx
+++ app/components/layout/TopBar.tsx
@@ -5,6 +5,9 @@ import Image from "next/image"
 import Link from "next/link"
 import { motion, AnimatePresence } from "framer-motion"
 import { useEffect, useState } from "react"
+const MotionLink = motion(Link)
+
+
 
 
 // navItems
@@ -33,62 +36,70 @@ export default function TopBar() {
       } else {
          document.body.style.overflow = ""
       }
+      return () => { document.body.style.overflow = "" }
    }, [isMenuOpen])
 
    return (
-      <nav className="bg-white border-b border-black/10 backdrop-blur-xl sticky z-50 top-0 flex flex-col lg:items-center">
-         <div className="flex py-3 text-xs items-center w-full lg:max-w-5xl text-zinc-900 justify-between">
+      <nav className="bg-white/80 dark:bg-black/80 border-b border-black/10 dark:border-white/10 backdrop-blur-xl sticky z-50 top-0 flex flex-col lg:items-center">
+         {/* container */}
+         <div className="flex py-3 text-xs items-center dark:text-white w-full lg:max-w-5xl text-zinc-900 justify-between">
             {/* logo */}
-            <div className="px-8"><Image className="size-6 lg:size-5" width={25} height={25} alt="web-icon" src="/logo/apple-logo.svg" /></div>
+            <div className={`px-8 transition-opacity duration-200 ${isMenuOpen ? " pointer-events-none opacity-0" : "opacity-100"}`}>
+               <Image className="size-6 dark:hidden lg:size-5" width={25} height={25} alt="web-icon" src="/logo/apple-logo.svg" />
+               <Image className="size-6 hidden dark:block lg:size-5" width={25} height={25} alt="web-icon" src="/logo/apple-logo-white.svg" />
+               </div>
             {/* items */}
             <div className="hidden lg:flex flex-1 justify-between">
                {navItems.map(item => <Link href={item.href} key={item.id}>{item.label}</Link>)}
             </div>
             {/* buttons */}
             <div className="flex gap-8 px-8">
-               <div className={`${isMenuOpen ? " pointer-events-none opacity-0" : "opacity-100"} flex transition-opacity duration-300 gap-8`}>
-                  <div><Search className="size-6 lg:size-4" /></div>
-                  <div><ShoppingBag className="size-6 lg:size-4" /></div>
+               <div className={`${isMenuOpen ? " pointer-events-none opacity-0" : "opacity-100"} flex transition-opacity duration-200 gap-8`}>
+                  <button type="button"><Search className="size-5  lg:size-4" /></button>
+                  <button type="button"><ShoppingBag className="size-5 lg:size-4" /></button>
                </div>
 
-               <div onClick={() => setIsMenuOpen(!isMenuOpen)} className="lg:hidden">
-                  {isMenuOpen ? <X className="size-6" /> : <Menu className="size-6" />}
-               </div>
+               <button type="button" onClick={() => setIsMenuOpen(!isMenuOpen)} className="lg:hidden">
+                  {isMenuOpen ? <X className="size-5" /> : <Menu className="size-5" />}
+               </button>
             </div>
          </div>
 
+         {/* Mobile Menu */}
          <AnimatePresence>
             {isMenuOpen && (
                <motion.div
                   initial={{ height: 0 }}
                   animate={{ height: "100vh" }}
                   exit={{ height: 0 }}
-                  transition={{ duration: 0.6, ease: [0.65, 0, 0.35, 1] }}
-                  className="fixed top-11.5 left-0 w-full bg-white overflow-hidden z-40 lg:hidden"
+                  transition={{ duration: 0.6 }}
+                  className=" fixed dark:bg-black top-11.5 left-0 w-full bg-white overflow-hidden z-40 lg:hidden"
                >
                   <motion.div
                      initial="hidden"
                      animate="show"
                      exit="hidden"
                      variants={{
-                        hidden: { transition: { staggerChildren: 0.05, staggerDirection: -1 } },
-                        show: { transition: { staggerChildren: 0.06, delayChildren: 0.3 } },
+                        hidden: { transition: { staggerChildren: 0.03, staggerDirection: -1 } },
+                        show: { transition: { staggerChildren: 0.04, delayChildren: 0.1 } },
                      }}
-                     className="flex flex-col text-black text-2xl pt-10 px-10"
+                     className="flex flex-col dark:text-white text-black gap-2 text-3xl pt-5 px-10"
                   >
+                     {/* Mobile Menu Items */}
                      {navItems.map((item) => (
-                        <motion.div
+                        <MotionLink
                            key={item.id}
+                           href={item.href}
+                           onClick={() => setIsMenuOpen(false)}
+                           className="font-extrabold flex items-start"
                            variants={{
                               hidden: { opacity: 0, y: -12 },
-                              show: { opacity: 1, y: 0 },
+                              show: { opacity: 1, y: 2 ,},
                            }}
-                           transition={{ duration: 0.35 }}
+                           transition={{ duration: 0.40}}
                         >
-                           <Link href={item.href} onClick={() => setIsMenuOpen(false)}>
-                              {item.label}
-                           </Link>
-                        </motion.div>
+                           {item.label}
+                        </MotionLink>
                      ))}
                   </motion.div>
                </motion.div>
