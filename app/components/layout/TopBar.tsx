3d014c5d39c8968491efede780789b5d5ba145f2
Kaveh <kavehkhorshidiii@gmail.com>
2026-07-28 17:30:28 +0330

    Developing part of the top bar

--- app/components/layout/TopBar.tsx
+++ app/components/layout/TopBar.tsx
@@ -3,11 +3,11 @@
 import { Search, ShoppingBag, Menu, X } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
-import { motion, AnimatePresence, delay } from "framer-motion"
-import { useState } from "react"
+import { motion, AnimatePresence } from "framer-motion"
+import { useEffect, useState } from "react"
 
 
-// navbar items
+// navItems
 export const navItems = [
    { id: 1, label: "Store", href: "/store" },
    { id: 2, label: "Mac", href: "/mac" },
@@ -27,87 +27,73 @@ export default function TopBar() {
 
    const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-   return (
-      <motion.nav
-         initial={{ height: "auto" }}
-         animate={{ height: isMenuOpen ? "100vh" : "46px" }}
-         transition={{ duration: 0.6, ease: [0.65, 0, 0.35, 1] }}
-         className=" bg-white border-b border-black/10 backdrop-blur-xl sticky z-50 top-0 flex flex-col lg:items-center overflow-hidden ">
+   useEffect(() => {
+      if (isMenuOpen) {
+         document.body.style.overflow = "hidden"
+      } else {
+         document.body.style.overflow = ""
+      }
+   }, [isMenuOpen])
 
+   return (
+      <nav className="bg-white border-b border-black/10 backdrop-blur-xl sticky z-50 top-0 flex flex-col lg:items-center">
          <div className="flex py-3 text-xs items-center w-full lg:max-w-5xl text-zinc-900 justify-between">
             {/* logo */}
-            <div className="px-8 "><Image className="size-6 lg:size-5" width={25} height={25} alt='web-icon' src='/logo/apple-logo.svg' /></div>
+            <div className="px-8"><Image className="size-6 lg:size-5" width={25} height={25} alt="web-icon" src="/logo/apple-logo.svg" /></div>
             {/* items */}
-            <div className=" hidden lg:flex flex-1 justify-between">
-               {navItems.map(item => <Link className="" href={item.href} key={item.id}>{item.label}</Link>)}
+            <div className="hidden lg:flex flex-1 justify-between">
+               {navItems.map(item => <Link href={item.href} key={item.id}>{item.label}</Link>)}
             </div>
             {/* buttons */}
             <div className="flex gap-8 px-8">
-               <div><Search className="size-6 lg:size-4" /></div>
-               <div><ShoppingBag className="size-6 lg:size-4" /></div>
-               {/* mobile menu button */}
-               <div onClick={() => setIsMenuOpen(!isMenuOpen)} className=" lg:hidden">{isMenuOpen ? <X className="size-6" /> : <Menu className="size-6" />}</div>
+               <div className={`${isMenuOpen ? " pointer-events-none opacity-0" : "opacity-100"} flex transition-opacity duration-300 gap-8`}>
+                  <div><Search className="size-6 lg:size-4" /></div>
+                  <div><ShoppingBag className="size-6 lg:size-4" /></div>
+               </div>
+
+               <div onClick={() => setIsMenuOpen(!isMenuOpen)} className="lg:hidden">
+                  {isMenuOpen ? <X className="size-6" /> : <Menu className="size-6" />}
+               </div>
             </div>
          </div>
 
          <AnimatePresence>
             {isMenuOpen && (
                <motion.div
-                  initial="hidden"
-                  animate="show"
-                  exit="hidden"
-                  variants={{
-                     hidden: {
-                        transition: {
-                           staggerChildren: 0.05,
-                           staggerDirection: -1, // از آخر به اول خارج میشن
-                        },
-                     },
-                     show: {
-                        transition: {
-                           staggerChildren: 0.06,
-                           delayChildren: 0.6,
-                        },
-                     },
-                  }}
-                  className="flex flex-col text-black text-2xl pt-10 px-10 lg:hidden"
+                  initial={{ height: 0 }}
+                  animate={{ height: "100vh" }}
+                  exit={{ height: 0 }}
+                  transition={{ duration: 0.6, ease: [0.65, 0, 0.35, 1] }}
+                  className="fixed top-11.5 left-0 w-full bg-white overflow-hidden z-40 lg:hidden"
                >
-                  {navItems.map((item) => (
-                     <motion.div
-                        key={item.id}
-                        variants={{
-                           hidden: {
-                              opacity: 0,
-                              y: -12,
-                           },
-                           show: {
-                              opacity: 1,
-                              y: 0,
-                           },
-                        }}
-                        transition={{
-                           duration: 0.35,
-                        }}
-                     >
-                        <Link href={item.href}>
-                           {item.label}
-                        </Link>
-                     </motion.div>
-                  ))}
+                  <motion.div
+                     initial="hidden"
+                     animate="show"
+                     exit="hidden"
+                     variants={{
+                        hidden: { transition: { staggerChildren: 0.05, staggerDirection: -1 } },
+                        show: { transition: { staggerChildren: 0.06, delayChildren: 0.3 } },
+                     }}
+                     className="flex flex-col text-black text-2xl pt-10 px-10"
+                  >
+                     {navItems.map((item) => (
+                        <motion.div
+                           key={item.id}
+                           variants={{
+                              hidden: { opacity: 0, y: -12 },
+                              show: { opacity: 1, y: 0 },
+                           }}
+                           transition={{ duration: 0.35 }}
+                        >
+                           <Link href={item.href} onClick={() => setIsMenuOpen(false)}>
+                              {item.label}
+                           </Link>
+                        </motion.div>
+                     ))}
+                  </motion.div>
                </motion.div>
             )}
          </AnimatePresence>
-
-      </motion.nav>
+      </nav>
    )
 }
-
-
-
-
-
-
-
-
-
-
