5a25d23a03801c5a6707bac8d8fa0d5cc386002d
Kaveh <kavehkhorshidiii@gmail.com>
2026-08-06 16:08:11 +0330

    commit

--- app/components/common/carousel/carousel.tsx
+++ app/components/common/carousel/carousel.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { useEffect, useState } from "react"
+import { ChevronRight, ChevronLeft, Equal } from 'lucide-react';
+
 
 interface CarouselProps<T> {
    items: T[];
@@ -9,22 +11,51 @@ interface CarouselProps<T> {
 
 export default function Carousel<T>({ items, renderItem }: CarouselProps<T>) {
 
+   const Duration = 3000
+
    const [currentIndex, setCurrentIndex] = useState(0)
+   const [isPaused, setIsPaused] = useState(false)
+   const [isHighlighted, setIsHighlighted] = useState(false)
+
    const nextIndex = (currentIndex + 1) % items.length
    const prevIndex = (currentIndex - 1 + items.length) % items.length
 
-   const handlePrev = () => setCurrentIndex(prevIndex)
-   const handleNext = () => setCurrentIndex(nextIndex)
+   const handlePrev = () => {
+      setCurrentIndex(prevIndex)
+      setIsHighlighted(true)
+      setTimeout(() => {
+         setIsHighlighted(false)
+      }, Duration)
+   }
+
+   const handleNext = () => {
+      setCurrentIndex(nextIndex)
+   }
+
+   const handlePauseToggle = () => {
+      setIsPaused(prev => !prev)
+      if (isPaused) {
+         setTimeout(() => { setCurrentIndex(nextIndex) }, 0);
+      }
+   }
+
+
 
    useEffect(() => {
+
+      if (isPaused) {
+         return;
+      }
+
       const intervalId = setInterval(() => {
          setCurrentIndex(nextIndex)
-      }, 3000);
+      }, Duration);
 
       return () => {
          clearInterval(intervalId)
       }
-   }, [currentIndex, nextIndex])
+
+   }, [currentIndex, nextIndex, isPaused])
 
    return (
       <div>
@@ -44,8 +75,12 @@ export default function Carousel<T>({ items, renderItem }: CarouselProps<T>) {
 
          {/* Controls */}
          <div className="flex justify-center gap-4 py-5">
-            <button onClick={handlePrev} className="border rounded-4xl px-4 py-2">prev</button>
-            <button onClick={handleNext} className="border rounded-4xl px-4 py-2">next</button>
+            
+            <button onClick={handlePrev} className={`border-2  rounded-full px-2 py-2 ${isHighlighted ? 'border-apple-blue' : 'border-apple-grow'}`}><ChevronLeft className=" size-5 " /></button>
+            <button onClick={handlePauseToggle} className={`border-2 rounded-full p-2 ${isPaused ? "border-apple-blue" : "border-apple-grow"}`}><Equal className=" size-5 rotate-90" /></button>
+            <div key={currentIndex} style={!isPaused ? { animationDuration: `${Duration}ms` } : {}} className="progress-ring p-0.5 rounded-4xl">
+               <button onClick={handleNext} className=" bg-apple-bg  rounded-full px-2 py-2"><ChevronRight className=" size-5" /></button>
+            </div>
          </div>
       </div>
    )
